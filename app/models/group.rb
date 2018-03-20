33938a0a4ee46aab3245febd92ff9a44deff377d
John <djbarbosa@uninorte.edu.co>
2018-03-20 07:43:15 -0500

    [feat]: Format downcase for the group and the members
    - The name of the groups are stored downcase
    - The alias of the members are stores downcase
    
    Resolves: #NA
    See also: #NA

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,11 @@
 class Group < ApplicationRecord
   has_many :members, dependent: :destroy
   has_many :users, :through => :members
+
+  before_save :format_downcase
+
+  protected
+  def format_downcase
+    self.name.downcase!
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,9 +3,14 @@ class Member < ApplicationRecord
   belongs_to :group
 
   after_destroy :delete_empty_group
+  before_save :format_downcase
 
   protected
   def delete_empty_group
     self.group.destroy if self.group.members.empty?
+  end
+
+  def format_downcase
+    self.alias.downcase!
   end
 end
