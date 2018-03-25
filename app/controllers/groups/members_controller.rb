222d3a739be4e50259753e2b9c1e91e6019ba077
John <djbarbosa@uninorte.edu.co>
2018-03-25 09:36:57 -0500

    [feat]: Features on members controller
    - Now the group admin can search a user by email or username and add it to the group
    - Now a non admin member of a group can update his alias and leave the
      group
    
    Resolves: #NA
    See also: #NA

--- app/controllers/groups/members_controller.rb
+++ app/controllers/groups/members_controller.rb
@@ -5,38 +5,56 @@ class MembersController < ApplicationController
   def show
     if is_a_group_member?
       render_ok @member.user
+    else
+      permissions_error
     end
-    permissions_error
   end
 
   def index
     if is_current_user_member?
       render_ok @group.members
+    else
+      permissions_error
     end
-    permissions_error
   end
 
   def create
     if is_group_admin?
-      member = Member.new({group_id:@group.id}.merge member_params)
-      save_and_render member
+      params[:email] ||= ''
+      params[:username] ||= ''
+      user = User.find_by(email: params[:email].downcase)
+      user = User.find_by(username: params[:username].downcase) unless user
+      if user
+        member = Member.new(group_id:@group.id, user_id:user.id, alias:params[:alias], admin:params[:admin])
+        save_and_render member
+      else
+        render json: {single_authentication: 'invalid credentials'}, status: :unauthorized 
+      end
+    else
+      permissions_error
     end
-    permissions_error
   end
 
   def update 
     if is_group_admin? and is_a_group_member?
       @member.update_attributes(alias:params[:alias], admin:params[:admin])
       save_and_render @member
+    elsif @member.user.id == @current_user.id
+      @member.update_attribute(:alias, params[:alias])
+      save_and_render @member
+    else
+      permissions_error
     end
-    permissions_error
   end
 
   def destroy
     if is_group_admin? and is_a_group_member?
       render_ok @member.destroy  
+    elsif @member.user.id == @current_user.id
+      render_ok @member.destroy  
+    else
+      permissions_error
     end
-    permissions_error
   end
 
   private 
@@ -58,13 +76,5 @@ class MembersController < ApplicationController
 
   def is_a_group_member?
     return @member.group.id == params[:group_id]
-  end
-
-  def member_params
-    params.permit(
-      :alias,
-      :user_id,
-      :admin
-    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,12 @@ Rails.application.routes.draw do
   post 'scraper/student_schedule', to: 'scraper#student_schedule'
   post 'scraper/conflict_matrix', to: 'scraper#conflict_matrix'
 
-  resources :groups, only: [:create, :update, :destroy]
-  get 'groups', to: 'groups#index'
-
-  resources :members, only: [:show, :create, :update, :destroy] 
-  post 'group_members', to: 'members#index'
+  resources :groups, only: [:index, :create, :update, :destroy]
+  # get 'groups', to: 'groups#index'
 
   namespace :groups do
     resources :members, only: [:show, :index, :create, :update, :destroy]
     # get ':group_id/members/:id', to: 'members#show'
-    # get ':group_id/members', to: 'members#index'
-    # post ':group_id/members', to: 'members#create'
   end
 
 end
