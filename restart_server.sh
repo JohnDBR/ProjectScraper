abc17fc9b1f79ad9c1f77343d8a87f3f371e6338
John Barbosa <djbarbosa@uninorte.edu.co>
2019-08-02 00:40:51 -0500

    RUN - git restart server old DNS!

--- restart_server.sh
+++ restart_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 whoami
 sudo kill -9  $(sudo lsof -t -i:3001)
-nohup rails s -b ec2-18-222-177-88.us-east-2.compute.amazonaws.com -p 3001 -P 2003 & #rails s -e production -d
+nohup rails s -p 3001 -P 2003 & #rails s -e production -d #-b ec2-18-222-177-88.us-east-2.compute.amazonaws.com
