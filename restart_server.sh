a69eb65ad70d41ee9dba52d566f223f8566bbd76
John Barbosa <djbarbosa@uninorte.edu.co>
2019-08-02 00:38:24 -0500

    RUN - git restar server modified

--- restart_server.sh
+++ restart_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 whoami
-sudo kill -9  $(sudo lsof -t -i:5005)
-nohup rails s -b ec2-18-222-177-88.us-east-2.compute.amazonaws.com -p 5005 -P 2003 & #rails s -e production -d
+sudo kill -9  $(sudo lsof -t -i:3001)
+nohup rails s -b ec2-18-222-177-88.us-east-2.compute.amazonaws.com -p 3001 -P 2003 & #rails s -e production -d
